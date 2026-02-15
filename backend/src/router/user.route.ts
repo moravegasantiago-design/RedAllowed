c100712fc7042acaa47910963b143cd819b075b1
moravegasantiago <moravegasantiago@gmail.com>
2026-02-14 23:45:36 -0500

    fix: resolve errors in user/profile update route
    Signed-off-by: moravegasantiago <moravegasantiago@gmail.com>

--- backend/src/router/user.route.ts
+++ backend/src/router/user.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { requestProfile, requestUser } from "../controllers/user.controllers";
+import {
+  requestProfile,
+  requestUpdate,
+  requestUser,
+} from "../controllers/user.controllers";
 
 const router = Router();
 
 router.post("/users", requestUser);
 router.post("/profile", requestProfile);
+router.post("/profile/update", requestUpdate);
 export default router;
